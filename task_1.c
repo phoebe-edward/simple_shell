d2cba72d3af3d8ad6db821b15a070e0ddfebfb06
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-08-23 06:21:24 -0700

    m

--- task_1.c
+++ task_1.c
@@ -7,36 +7,36 @@
  */
 void executeCommand(const char *command)
 {
-        if (access(command, X_OK) == 0)
-        {
-        pid_t pid = fork();
+	if (access(command, X_OK) == 0)
+	{
+	pid_t pid = fork();
 
-        if (pid < 0)
-        {
-        perror("Forking failed");
-        return;
-        }
-        else if (pid == 0)
-        {
-        char *args[] = {(char *)command, NULL};
+	if (pid < 0)
+	{
+	perror("Forking failed");
+	return;
+	}
+	else if (pid == 0)
+	{
+	char *args[] = {(char *)command, NULL};
 
-        if (execve(command, args, NULL) == -1)
-        {
-        perror("Error executing command");
-        exit(EXIT_FAILURE);
-        }
-        }
-        else
-        {
-        int status;
+	if (execve(command, args, NULL) == -1)
+	{
+	perror("Error executing command");
+	exit(EXIT_FAILURE);
+	}
+	}
+	else
+	{
+	int status;
 
-        waitpid(pid, &status, 0);
-        }
-        }
-        else
-        {
-        printf("%s: No such file or directory\n", command);
-        }
+	waitpid(pid, &status, 0);
+	}
+	}
+	else
+	{
+	printf("%s: No such file or directory\n", command);
+	}
 }
 
 /**
@@ -45,27 +45,27 @@ void executeCommand(const char *command)
  */
 int prompt(void)
 {
-        char *command = NULL;
-        size_t len = 0;
-        ssize_t read;
+	char *command = NULL;
+	size_t len = 0;
+	ssize_t read;
 
-        while (1)
-        {
-        if (isatty(STDIN_FILENO))
-        printf("#cisfun$ ");
-        read = getline(&command, &len, stdin);
+	while (1)
+	{
+	if (isatty(STDIN_FILENO))
+	printf("#cisfun$ ");
+	read = getline(&command, &len, stdin);
 
-        if (read == -1)
-        {
-        printf("\n");
-        break;/* EOF (Ctrl+D)*/
-        }
-        if (read > 0 && command[read - 1] == '\n')
-        {
-        command[read - 1] = '\0';
-        }
-        executeCommand(command);
-        }
-        free(command);
-        return (0);
+	if (read == -1)
+	{
+	printf("\n");
+	break;/* EOF (Ctrl+D)*/
+	}
+	if (read > 0 && command[read - 1] == '\n')
+	{
+	command[read - 1] = '\0';
+	}
+	executeCommand(command);
+	}
+	free(command);
+	return (0);
 }
