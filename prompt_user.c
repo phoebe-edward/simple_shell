ba1d8696c3c2ae96b22807aa0844d82e01bb5e73
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-08-23 00:20:32 -0700

    m

--- prompt_user.c
+++ prompt_user.c
@@ -1,15 +1,70 @@
 #include "shell.h"
+/**
+ * executeCommand - Executes given command, managing child process,
+ * permissions, and errors.
+ * @command: Command to execute
+ */
+void executeCommand(const char *command)
+{
+        if (access(command, X_OK) == 0)
+        {
+        pid_t pid = fork();
+
+        if (pid < 0)
+        {
+        perror("Forking failed");
+        return;
+        }
+        else if (pid == 0)
+        {
+        char *args[] = {(char *)command, NULL};
+
+        if (execve(command, args, NULL) == -1)
+        {
+        perror("Error executing command");
+        exit(EXIT_FAILURE);
+        }
+        }
+        else
+        {
+        int status;
+
+        waitpid(pid, &status, 0);
+        }
+        }
+        else
+        {
+        printf("%s: No such file or directory\n", command);
+        }
+}
 
 /**
-* prompt_user - prints $ to let user know the program is
-* ready to take their input
-* prints the prompt if the shell is in interactive mode
-* Return: no return
-*/
-void prompt_user(void)
+ * prompt - Displays a prompt and executes entered commands.
+ * Return: Always 0
+ */
+int prompt_user(void)
 {
-	if ((isatty(STDIN_FILENO) == 1) && (isatty(STDOUT_FILENO) == 1))
-		flags.interactive = 1;
-	if (flags.interactive)
-		write(STDERR_FILENO, "$ ", 2);
+        char *command = NULL;
+        size_t len = 0;
+        ssize_t read;
+
+        while (1)
+        {
+        if (isatty(STDIN_FILENO))
+        printf("#cisfun$ ");
+        read = getline(&command, &len, stdin);
+
+        if (read == -1)
+        {
+        printf("\n");
+        break;/* EOF (Ctrl+D)*/
+        }
+        if (read > 0 && command[read - 1] == '\n')
+        {
+        command[read - 1] = '\0';
+        }
+        executeCommand(command);
+        }
+        free(command);
+        return (0);
 }
