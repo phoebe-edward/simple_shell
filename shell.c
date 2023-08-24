31bdfebd50ccf62df688076ed8aefed41b908e04
ibrahimbachiri <ibrahimbachiri205@gmail.com>
2023-08-23 17:21:32 -0700

    m

--- shell.c
+++ shell.c
@@ -1,50 +1,16 @@
 #include "shell.h"
 /**
-* main - carries out the read, execute then print output loop
-* @ac: argument count
-* @av: argument vector
-* @envp: environment vector
-*
-* Return: 0
-*/
-
-int main(int ac, char **av, char *envp[])
+ * main - main function that sets shell loop function
+ * @ac: argument count/not set
+ * @av: argument vector
+ * Return: Exits if successful.
+ */
+int main(int ac, char **av)
 {
-	char *line = NULL, *pathcommand = NULL, *path = NULL;
-	size_t bufsize = 0;
-	ssize_t linesize = 0;
-	char **command = NULL, **paths = NULL;
-	(void)envp, (void)av;
-	if (ac < 1)
-		return (-1);
-	signal(SIGINT, handle_signal);
-	while (1)
-	{
-		free_buffers(command);
-		free_buffers(paths);
-		free(pathcommand);
-		prompt_user();
-		linesize = getline(&line, &bufsize, stdin);
-		if (linesize < 0)
-			break;
-		info.ln_count++;
-		if (line[linesize - 1] == '\n')
-			line[linesize - 1] = '\0';
-		command = tokenizer(line);
-		if (command == NULL || *command == NULL || **command == '\0')
-			continue;
-		if (checker(command, line))
-			continue;
-		path = find_path();
-		paths = tokenizer(path);
-		pathcommand = test_path(paths, command[0]);
-		if (!pathcommand)
-			perror(av[0]);
-		else
-			execution(pathcommand, command);
-	}
-	if (linesize < 0 && flags.interactive)
-		write(STDERR_FILENO, "\n", 1);
-	free(line);
-	return (0);
+ac = ac;
+av = av;
+printf("\t********** Welcome to Our New custom C-Shell **********\n\n");
+printf("\t\t********** Made for ALX Kenya **********\n\n");
+shell_loop();
+return (EXIT_SUCCESS);
 }
--- shell.h
+++ shell.h
@@ -1,61 +1,26 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include <stdarg.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
 #include <sys/wait.h>
-#include <sys/stat.h>
-#include <time.h>
-#include <stdbool.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#define TOK_BUFSIZE 64
+#define TOK_DELIM " \t\r\n\a"
 
-/* environment variables */
-extern char **environ;
-extern __sighandler_t signal(int __sig, __sighandler_t __handler);
-
-/* handle built ins */
-int checker(char **cmd, char *buf);
-void prompt_user(void);
-void handle_signal(int m);
-char **tokenizer(char *line);
-char *test_path(char **path, char *command);
-char *append_path(char *path, char *command);
-int handle_builtin(char **command, char *line);
-void exit_cmd(char **command, char *line);
-
-void print_env(void);
-
-/* string handlers */
-int _strcmp(char *s1, char *s2);
-int _strlen(char *s);
-int _strncmp(char *s1, char *s2, int n);
-char *_strdup(char *s);
-char *_strchr(char *s, char c);
-
-void execution(char *cp, char **cmd);
-char *find_path(void);
-
-/* helper function for efficient free */
-void free_buffers(char **buf);
-
-struct builtin
-{
-	char *env;
-	char *exit;
-} builtin;
-
-struct info
-{
-	int final_exit;
-	int ln_count;
-} info;
-
-struct flags
-{
-	bool interactive;
-} flags;
-
-#endif /* SHELL_H */
+int main(int ac, char **av);
+int shell_cd(char **args);
+int shell_exit(char **args);
+int num_builtins(void);
+int launch(char **args);
+char *read_line(void);
+int execute(char **args);
+void shell_loop(void);
+char **split_line(char *line);
+char *_getenv(char *name);
+char *_which(char *str1, char *str2);
+int _path(char **argv);
+#endif
